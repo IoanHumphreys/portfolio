e9c0806b052d08213298b9284177a0d0d270a56c
Ioan Humphreys <106632705+IoanHumphreys@users.noreply.github.com>
2023-08-22 20:41:06 +0100

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -193,14 +193,14 @@ export default function Home() {
                   </div>
                 </div>
 
-                {/* MongoDB */}
+                {/* Java */}
                 <div className="flex items-center bg-panels-gray p-2 rounded-lg">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
                   </svg>
                   <div>
-                    <h3 className="text-lg font-medium">MongoDB</h3>
-                    <p className="text-sm">Intermediate</p>
+                    <h3 className="text-lg font-medium">Java</h3>
+                    <p className="text-sm">Basic</p>
                   </div>
                 </div>
 
