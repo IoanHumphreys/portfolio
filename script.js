0d78e5f8b89401699f37345410c750200066d6e2
Ioan Humphreys <106632705+IoanHumphreys@users.noreply.github.com>
2023-10-05 18:52:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// JavaScript to trigger the fade-in animation on scroll
+function scrollFade() {
+    const elements = document.querySelectorAll('.scroll-fade');
+
+    elements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (elementTop < windowHeight) {
+            element.classList.add('fade-in');
+        }
+    });
+}
+
+// Event listener to trigger scrollFade function on scroll
+window.addEventListener('scroll', scrollFade);
+
+// Initialize the scrollFade function when the page loads
+window.addEventListener('DOMContentLoaded', () => {
+    scrollFade();
+});
+
+function scrollToSection(id) {
+    const section = document.querySelector(id);
+    if (section) {
+        window.scrollTo({
+            behavior: 'smooth',
+            top: section.offsetTop
+        });
+    }
+}
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default link behavior
+        const targetId = this.getAttribute('href');
+        scrollToSection(targetId);
+    });
+});
